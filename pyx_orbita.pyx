f56048c32f46b334e68009565efdb5c5383d8e03
seb03990 <109989087+seb03990@users.noreply.github.com>
2022-11-01 07:28:55 -0500

    Add files via upload

---
+++ pyx_orbita.pyx
@@ -0,0 +1,39 @@
+#cython: language_level=3
+from math import sqrt
+
+class Planet(object):
+	
+	def __init__(self):
+		
+		#posicion y velocidad inicial
+		self.x = 1.0
+		self.y = 0.0
+		self.z = 0.0
+		self.vx = 0.0
+		self.vy = 0.5
+		self.vz = 0.0
+		
+		#masa
+		self.m = 1.0
+		
+	def single_step(self, object planet, float dt):
+		
+		cdef float distance = sqrt(planet.x**2 + planet.y**2  + planet.z**2)
+		
+		cdef float Fx = -planet.x / distance**3
+		cdef float Fy = -planet.y / distance**3
+		cdef float Fz = -planet.z / distance**3
+		
+		planet.vx += dt * Fx / planet.m
+		planet.vy += dt * Fy / planet.m
+		planet.vz += dt * Fz / planet.m
+		
+	def step_time(self, object planet, int time_span,int n_steps):
+		
+		cdef float dt = 0.0
+		cdef int j
+		
+		dt = time_span / n_steps
+		
+		for j in range(n_steps):
+			self.single_step(planet,dt)
